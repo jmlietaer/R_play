d482d19e70569f191e7c19f61988af40b413d28c
jmlietaer <github.jm@lietaer.be>
2015-05-16 10:22:19 +0200

    est
    test

--- leaflet/leaflet.R
+++ leaflet/leaflet.R
@@ -17,7 +17,7 @@ library(ggmap)
 
 x <- geocode(c("koningsstraat 100, 1000 Brussel"), output="more")
 
-leaflet() %>% addTiles() %>% setView(x$lon, x$lat, zoom = 18) 
+leaflet() %>% addTiles() %>% setView(x$lon, x$lat, zoom = 18) %>% addMarkers(data = x, lng = x$lon, lat = x$lat, popup = "Popup, <b>bold text</b>") %>% addPopups(x$lon, x$lat, "Popup, <b>bold text</b>")
 
 # leaflet() %>% addTiles() %>% setView(-84.3847, 33.7613, zoom = 16) %>%
 #  +     addMarkers(data = atlanta, lat = ~ Latitude, lng = ~ Longitude,popup = atlanta$Name) %>%
