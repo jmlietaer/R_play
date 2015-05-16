ea89c69ec1f494293cf0145eb6659309f4fa1013
jmlietaer <github.jm@lietaer.be>
2015-05-16 10:29:48 +0200

    test
    working version
    * map tile
    * marker with text
    * popup text

--- leaflet/leaflet.R
+++ leaflet/leaflet.R
@@ -1,7 +1,9 @@
-
-
-
-
+#
+#
+#
+#
+#   Source:   http://www.computerworld.com/article/2894448/useful-new-r-packages-for-data-visualization-and-analysis.html
+#
 
 # install.packages("devtools")
 # devtools::install_github("rstudio/leaflet")
@@ -13,13 +15,8 @@ library(dplyr)
 # install.packages("ggmap")
 library(ggmap)
 
-
-
 x <- geocode(c("koningsstraat 100, 1000 Brussel"), output="more")
 
-leaflet() %>% addTiles() %>% setView(x$lon, x$lat, zoom = 18) 
-
-# leaflet() %>% addTiles() %>% setView(-84.3847, 33.7613, zoom = 16) %>%
-#  +     addMarkers(data = atlanta, lat = ~ Latitude, lng = ~ Longitude,popup = atlanta$Name) %>%
-#  +     addPopups(-84.3847, 33.7616, 'Data journalists at work, <b>NICAR 2015</b>')
+# leaflet() %>% addTiles() %>% setView(x$lon, x$lat, zoom = 18) 
+leaflet() %>% addTiles() %>% setView(x$lon, x$lat, zoom = 18) %>% addMarkers(data = x, lng = x$lon, lat = x$lat, popup = "Popup, <b>bold text</b>") %>% addPopups(x$lon, x$lat, "Popup, <b>bold text</b>")
 
