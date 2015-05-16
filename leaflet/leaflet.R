11e2297ae07a3455a59e50fbbd1bd1dbc623137f
jmlietaer <github.jm@lietaer.be>
2015-05-16 13:11:09 +0200

    pm
    pm

--- leaflet/leaflet.R
+++ leaflet/leaflet.R
@@ -21,8 +21,7 @@ library(ggmap)
 
 x <- geocode(c("paleizenplein, 1000 Brussel"), output="more")
 
-# leaflet() %>% addTiles() %>% setView(x$lon, x$lat, zoom = 18) 
+# leaflet() %>% addTiles() %>% setView(x$, x$lat, zoom = 18) 
 leaflet() %>% addTiles() %>% setView(x$lon, x$lat, zoom = 16) %>% addMarkers(data = x, lng = x$lon, lat = x$lat, popup = "Brussels<br/><b>Royal Palace</b>") %>% addPopups(x$lon, x$lat, "<center>Brussels<br/><b>Royal Palace</b></center>")
-
 
 
