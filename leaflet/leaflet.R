b6dca34c87defdb08531d47df5643e6aafdc3270
jmlietaer <github.jm@lietaer.be>
2015-05-16 11:48:35 +0200

    test html
    markup center br

--- leaflet/leaflet.R
+++ leaflet/leaflet.R
@@ -1,6 +1,8 @@
 #
+#   Author:   J.M. Lietaer
+#   Date:     20150516
 #
-#
+#   Subject:  Test - Libraries leaflet, dplyr, ggmap
 #
 #   Source:   http://www.computerworld.com/article/2894448/useful-new-r-packages-for-data-visualization-and-analysis.html
 #
@@ -15,8 +17,9 @@ library(dplyr)
 # install.packages("ggmap")
 library(ggmap)
 
-x <- geocode(c("koningsstraat 100, 1000 Brussel"), output="more")
+x <- geocode(c("paleizenplein, 1000 Brussel"), output="more")
 
 # leaflet() %>% addTiles() %>% setView(x$lon, x$lat, zoom = 18) 
-leaflet() %>% addTiles() %>% setView(x$lon, x$lat, zoom = 18) %>% addMarkers(data = x, lng = x$lon, lat = x$lat, popup = "Popup, <b>bold text</b>") %>% addPopups(x$lon, x$lat, "Popup, <b>bold text</b>")
+leaflet() %>% addTiles() %>% setView(x$lon, x$lat, zoom = 16) %>% addMarkers(data = x, lng = x$lon, lat = x$lat, popup = "Brussels<br/><b>Royal Palace</b>") %>% addPopups(x$lon, x$lat, "<center>Brussels<br/><b>Royal Palace</b></center>")
+
 
