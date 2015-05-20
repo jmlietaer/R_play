a569f5f54dd1107ab6200e612ffa8d655a31483d
jmlietaer <github.jm@lietaer.be>
2015-05-20 14:01:56 +0200

    added provider tiles
    test

--- leaflet/leaflet2.R
+++ leaflet/leaflet2.R
@@ -1,6 +1,6 @@
 #
 #   Author:   J.M. Lietaer - https://github.com/jmlietaer
-#   Date:     20150518
+#   Date:     20150520
 #   Subject:  Test - Libraries leaflet - Data imported from csv-file
 #
 #   Source(s):   
@@ -11,6 +11,7 @@
 #         ggplot2. The R Journal, 5(1), 144-161. URL
 #         http://journal.r-project.org/archive/2013-1/kahle-wickham.pdf
 #   [4]   Get Latitude Longitude - http://www.latlong.net/search.php?keyword=
+#   [5]   leaflet providers - http://leaflet-extras.github.io/leaflet-providers/preview/index.html
 # 
 
 # library(ggmap) # only needed for "Test with ggmap"
@@ -36,8 +37,10 @@ library(leaflet)
 ### START ### Test with leaflet ###
 
 file_in = "coordinates.csv"
+
 pop = read.csv(file_in, header=F, stringsAsFactors=F, skip=0, sep=";")
-A = matrix(c(pop[,1], pop[,2], pop[,3]), nrow=3, ncol=3, byrow = FALSE) 
+
+A = matrix(c(pop[,1], pop[,2], pop[,3]), nrow=, ncol=3, byrow = FALSE) 
 
 ### END ### Test with leaflet ###
 
@@ -46,15 +49,35 @@ A = matrix(c(pop[,1], pop[,2], pop[,3]), nrow=3, ncol=3, byrow = FALSE)
 x <- mean(as.numeric(A[,1]))
 y <- mean(as.numeric(A[,2]))
 
-# ???
-# z_lng <- round(abs(max(as.numeric(A[,1]))-min(as.numeric(A[,1])))+.5)
-# z_lat <- round(abs(max(as.numeric(A[,2]))-min(as.numeric(A[,2])))+.5)
-# z <- z_lat * 2
-# if (z_lng > z_lat) {z <- z_lng * 2}
-
+## Define the map
 m = leaflet() %>% addTiles() %>% setView(x, y, zoom = 1)
-m = m %>% addMarkers(data = A, lng = A[,1], lat = A[,2], icon = icons(iconUrl = "http://a.fsdn.com/allura/p/freemind/icon", iconWidth = 30, iconHeight = 30), popup = paste(A[,3], "<br/>", A[,1], "<br/>", A[,2], sep=""))
+
+## Ass provider tiles to the map
+# m = m %>% addProviderTiles("OpenStreetMap.HOT")
+# m = m %>% addProviderTiles("OpenStreetMap.BlackAndWhite")
+# m = m %>% addProviderTiles("Stamen.Toner")
+# m = m %>% addProviderTiles("Stamen.Watercolor")
+# m = m %>% addProviderTiles("Acetate.terrain")
+# m = m %>% addProviderTiles("CartoDB.Positron")
+# m = m %>% addProviderTiles("Esri.WorldTopoMap")
+# m = m %>% addProviderTiles("Acetate.all")
+m = m %>% addProviderTiles("HikeBike.HillShading")                                             
+
+## Add pop-ups to the map
+m = m %>% addMarkers(data = A, lng = A[,1], lat = A[,2], icon = icons(iconUrl = "icons/man.png", iconWidth = 30, iconHeight = 30), popup = paste(A[,3], "<br/>lon = ", A[,1], "<br/>lat = ", A[,2], sep=""))
+
+## Add pop-ups to the map
 m = m %>% addPopups(data = A, lng = A[,1], lat = A[,2], popup = A[,3])
+
+## Plot the map
 m
+
+## REMARK
+# In case you're not familiar with the magrittr pipe operator (%>%), 
+# here is the equivalent without using pipes:
+# m <- leaflet()
+# m <- addTiles(m)
+# m <- addMarkers(m, lng=174.768, lat=-36.852, popup="The birthplace of R")
+# m
 
 ### END ### Common code ###
